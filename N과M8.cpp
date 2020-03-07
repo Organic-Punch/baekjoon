d60632ccf8eaaac1b12ae861914430d36d3dface
taeyong <sinho667@naver.com>
2020-03-07 21:47:17 +0900

    NandM8

---
+++ N과M8.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int arr[8];
+void Ios() { ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); }
+
+int N, M;
+
+void B(int idx, vector<int>s) {
+	if (s.size() == M) {
+		for (int i = 0; i < s.size(); i++)
+			cout << s[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	else {
+		for (int i = idx; i <= N; i++) {
+			s.push_back(arr[i]);
+			B(i, s);
+			s.pop_back();
+		}
+	}
+}
+
+int main() {
+	Ios();
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) cin >> arr[i];
+	sort(arr + 1, arr + N + 1);
+	vector<int>s;
+	B(1, s);
+	return 0;
+}
