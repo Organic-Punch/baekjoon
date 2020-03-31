529082fdd4e6b45b4c1624efaee76b8d5c526227
taeyong <sinho667@naver.com>
2020-04-01 02:45:14 +0900

    2020-04-01

--- 숫자고르기.cpp
+++ 숫자고르기.cpp
@@ -1,10 +1,32 @@
 #include <cstdio>
-
+#include <vector>
+#include <algorithm>
+#pragma warning (disable : 4996)
 using namespace std;
 
-int arr[101];
+int N;
+vector<int> arr;
+vector<int> ans;
+
+void dfs(int S, int C,vector<int>vis) {
+    if (vis[C]) {
+        if (C == S)
+            ans.push_back(S);
+    }
+    else {
+        vis[C]++;
+        dfs(S, arr[C],vis);
+    }
+}
 
 int main() {
-	int N; scanf("%d")
-	return 0;
+    scanf("%d", &N);
+    arr.resize(N + 1, 0);
+    vector<int> vis(N + 1, 0);
+    for (int i = 1; i <= N; i++) scanf("%d", &arr[i]);
+    for (int i = 1; i <= N; i++) 
+        dfs(i, i, vis);
+    printf("%d\n", ans.size());
+    for (int i = 0; i < ans.size(); i++) printf("%d\n", ans[i]);
+    return 0;
 }
